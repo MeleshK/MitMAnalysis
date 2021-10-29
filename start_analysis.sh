2be3941e155c4d4e31078acdd695a68c46b1a90f
Melesh <melesh@knight22.com>
2021-10-29 09:04:00 +0100

    tidy up and commenting of analysis

--- start_analysis.sh
+++ start_analysis.sh
@@ -1,16 +1,10 @@
 #!/bin/bash
-
-mitmweb --anticomp --anticache --ignore-hosts '^(?:(?!google).)*$' --set block_global=false --set flow_detail=3 --mode transparent --showhost --web-host 0.0.0.0 --web-port 9090 --no-web-open-browser --ssl-insecure --no-web-open-browser --verbose -s mitmdecode.py > stream.txt &
-
-#Every 10  seconds, put stream into capture
-#Then empty the contents to stream
-while [ 0 -lt 1 ]
-do
-	sleep 10s
-	cat stream.txt >> capture.txt
-	tr < capture.txt -d '\000' > capturefixed.txt
-	cat capturefixed.txt >> backup.txt
-	echo "" > stream.txt # empty stream file
-	python3 ./AnalysisMain.py
-	echo "" > capture.txt # empty capture file
-done
+printf "$1 copying to capture.txt"
+cat $1 >> capture.txt
+printf "Removing '\000'"
+tr < capture.txt -d '\000' > capturefixed.txt
+printf "Taking backup"
+cat capturefixed.txt >> backup.txt
+printf "Performing analysis"
+python3 ./AnalysisMain.py
+printf "Analysis Complete"
