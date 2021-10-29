35106e61a234f76f3440ae85d78143b72733890a
Melesh <melesh@knight22.com>
2021-10-29 16:27:12 +0100

    update store directory for capture files

--- runserver.sh
+++ runserver.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-mitmweb --anticomp --anticache --ignore-hosts '^(?:(?!google).)*$' --set block_global=false --set flow_detail=3 --mode transparent --showhost --web-host 0.0.0.0 --web-port 9090 --save-stream-file ~/mitmproxy-`date +%s`-flow.cap --ssl-insecure --no-web-open-browser --ssl-insecure --no-web-open-browser --verbose -s mitmdecode.py &
+mitmweb --anticomp --anticache --ignore-hosts '^(?:(?!google).)*$' --set block_global=false --set flow_detail=3 --mode transparent --showhost --web-host 0.0.0.0 --web-port 9090 --save-stream-file /project/mitmproxy-`date +%s`-flow.cap --ssl-insecure --no-web-open-browser --ssl-insecure --no-web-open-browser --verbose -s mitmdecode.py &
