9fe102fd23b1b51cc83e6020a7a7a41742de6fd3
Melesh <melesh@knight22.com>
2021-10-30 11:02:35 +0100

    update assemble to make

--- mitmdecode.py
+++ mitmdecode.py
@@ -2,8 +2,10 @@ from mitmproxy import flowfilter
 from mitmproxy import ctx, http
 
 def response(flow):
-		flow.response.content = flow.response.assemble
+	ctx.log.info("Response detected")
+	flow.response.content = flow.response.make
 
 def request( flow):
-		flow.request.content = flow.request.assemble
+	ctx.log.info("Request detected")
+	flow.request.content = flow.request.make
 
