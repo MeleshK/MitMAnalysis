6a1ea5476fdb0b448df90529aecdf4ed9d5aeb5a
Melesh <melesh@knight22.com>
2021-10-30 11:32:27 +0100

    update to decode

--- mitmdecode.py
+++ mitmdecode.py
@@ -1,11 +1,12 @@
-from mitmproxy import flowfilter
 from mitmproxy import ctx, http
 
-def response(flow):
+def request(self, flow: http.HTTPFlow) -> None:
+	flow.request.content = flow.request.make
+	ctx.log.info("Request detected")
+
+def response(self, flow: http.HTTPFlow) -> None:
 	ctx.log.info("Response detected")
 	flow.response.content = flow.response.make
 
-def request( flow):
-	ctx.log.info("Request detected")
-	flow.request.content = flow.request.make
+
 
