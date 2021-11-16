2d7e90aaaa5e6020e80062bd9110b01d351946e4
MeleshK <51149294+MeleshK@users.noreply.github.com>
2021-11-16 13:44:25 +0000

    Update runserver.sh

--- runserver.sh
+++ runserver.sh
@@ -1,2 +1,25 @@
 #!/bin/bash
-mitmweb --anticomp --anticache --set block_global=false --set flow_detail=3 --mode transparent --showhost --web-host 0.0.0.0 --web-port 9090 --save-stream-file /project/mitmproxy-`date +%s`-flow.cap --ssl-insecure --no-web-open-browser --ssl-insecure --no-web-open-browser --verbose -s mitmdecode.py &
+
+#terminate mitmdump if already running.
+try:
+  kill "$(pgrep mitmdump)"
+
+echo "" > stream.txt
+echo "" > capture.txt
+now=$(date +"%Y_%m_%d_%H_%M")
+
+#Starts mitmdump and outputs to output.txt
+mitmdump --anticomp --anticache --set block_global=false --set flow_detail=3 --mode transparent --showhost --save-stream-file "/project/mitmproxy_$now.cap" --set stream_large_bodies=5m --ssl-insecure --verbose -s mitmdecode.py &
+
+#Every 10  seconds, put stream into capture
+#Then empty the contents to stream
+while [ 0 -lt 1 ]
+do
+	sleep 10s
+	cat stream.txt >> capture.txt
+	tr < capture.txt -d '\000' > capturefixed.txt
+	cat capturefixed.txt >> "backup-$now.txt"
+	echo "" > stream.txt
+	python3 ./AnalysisMain.py
+	echo "" > capture.txt
+done
